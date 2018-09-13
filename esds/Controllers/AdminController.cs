8807324459031d1c6854492efc7d1e23fc7de03b
Onur <Onur@DESKTOP-65SDM5S>
2018-09-13 13:37:09 +0300

    Add project files.

---
+++ esds/Controllers/AdminController.cs
@@ -0,0 +1,113 @@
+﻿using esds.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace esds.Controllers
+{
+    public class AdminController : Controller
+    {
+        EsdsEntities1 baglanti = new EsdsEntities1();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Urunler()
+        {
+            return View(baglanti.Urun.ToList());
+        }
+        public ActionResult UrunEklee()
+        {
+            ViewBag.Kategoriler = baglanti.Kategori.ToList();
+            ViewBag.Markalar = baglanti.Marka.ToList();
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult UrunEklee(Urun urun)
+        {
+          
+                baglanti.Urun.Add(urun);
+                baglanti.SaveChanges();
+                return View();
+           
+
+        }
+        public ActionResult Markalar() {
+
+            return View(baglanti.Marka.ToList());
+
+        }
+        public ActionResult MarkaEkle()
+        {
+
+            return View();
+
+        }
+        public ActionResult UrunSil(int id) {
+            Urun urn = (from u in baglanti.Urun where u.Id == id select u).FirstOrDefault();
+            return View(urn);
+
+       }
+        [HttpPost,ActionName("UrunSil")]
+        public ActionResult UrunSilme(int idd) {
+            Urun urn = (from u in baglanti.Urun where u.Id == idd select u).FirstOrDefault();
+            int Id = urn.Id;
+            baglanti.Urun.Remove(urn);
+            baglanti.SaveChanges();
+            return RedirectToAction("Urunler", new { id = Id });
+        }
+        public ActionResult MarkaGuncelle(int id)
+        {
+            Marka mrk = baglanti.Marka.Find(id);
+            
+            return View(mrk);
+
+        }
+        
+        [HttpPost]
+
+        public ActionResult MarkaEkle(Marka mrk)
+        {
+            try
+            {
+                baglanti.Marka.Add(mrk);
+                baglanti.SaveChanges();
+                return View();
+            }
+            catch { return View();
+ }
+           
+        }
+        public ActionResult Kategoriler()
+        {
+            return View(baglanti.Kategori.ToList());
+
+
+
+        }
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori kategori)
+        {
+            try
+            {
+                baglanti.Kategori.Add(kategori);
+                baglanti.SaveChanges();
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ esds/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using esds.Models;
+
+namespace esds.Controllers
+{
+    public class HomeController : Controller
+    {
+        EsdsEntities2 baglanti = new EsdsEntities2();
+
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Uye uye)
+        {
+            var login = baglanti.Uye.Where(u => u.KullaniciAdi == uye.KullaniciAdi).SingleOrDefault();
+            if (login.KullaniciAdi == uye.KullaniciAdi && login.Sifre==uye.Sifre) 
+            {
+                Session["uyeid"] = login.UyeId;
+                Session["uyeadi"] = login.KullaniciAdi;
+                Session["yetkiid"] = login.YetkiId;
+                return RedirectToAction("Index","Admin");
+            }
+            else
+            {
+                ModelState.AddModelError("KullaniciAdi","Lütfen Yeni Kullanıcı Oluşturunuz");
+            return View();
+
+            }
+        }
+        public ActionResult LogOut() {
+            Session["uyeid"] = null;
+            Session.Abandon();
+                return RedirectToAction("Login","Home");
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Uye uye)
+        {
+            if(ModelState.IsValid)
+            {
+                uye.YetkiId = 2;
+                baglanti.Uye.Add(uye);
+                baglanti.SaveChanges();
+                Session["uyeid"] = uye.UyeId;
+                Session["kullaniciadi"] = uye.KullaniciAdi;
+                return RedirectToAction("Login","Home");
+            }
+            return View();
+        }
+    }
+}
